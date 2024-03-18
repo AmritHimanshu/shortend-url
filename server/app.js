de368a7ea8461ab139d991b928826e771206d9d0
AmritHimanshu <amrit.ewp@gmail.com>
2024-03-18 13:46:19 +0530

    changed config location

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cors({
-    origin:'http://localhost:3000'
+    origin:'https://shortend-url.vercel.app'
 }));
 
 app.use(require('./route/url'));
